cc9abed92f985fd1de84aed6ef082b01fd842d5d
Hrihorii Ilin <devopsick@pm.me>
2023-09-20 16:21:38 +0000

    +

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
@@ -8,7 +8,7 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({
+  adapter: vercelServerless({
     imageService: true,
     webAnalytics: {
       enabled: true
